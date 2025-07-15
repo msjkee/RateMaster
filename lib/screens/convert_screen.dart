dfaf2168be9335b0ed4efb225e27865ff0c2b0b1
Misha (MacBook) <stukalov.2004@inbox.ru>
2025-07-15 20:20:39 +0300

    Settings and Convert screens creation

---
+++ lib/screens/convert_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConvertScreen extends StatefulWidget {
+  const ConvertScreen({super.key});
+
+  @override
+  State<ConvertScreen> createState() => _ConvertScreenState();
+}
+
+class _ConvertScreenState extends State<ConvertScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
